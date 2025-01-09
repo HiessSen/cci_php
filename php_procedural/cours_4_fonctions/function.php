68061a475129e4c6678a196d26aa6feb2302e4f1
HiessSen <118723696+HiessSen@users.noreply.github.com>
2025-01-09 14:44:00 +0100

    second commit

--- php_procedural/cours_4_fonctions/function.php
+++ php_procedural/cours_4_fonctions/function.php
@@ -59,21 +59,37 @@ function genererMotDePasse($param=array())
     // Je vérifie si $nbCarac est bien inferieur ou égal à la longueur
     if ($nbCarac <= $param['longueur'])
     {
-        // Je fais une boucle pour sélectionner les caractères
+        // Je fais une boucle pour sélectionner les caractères spéciaux
         for ($i = 0; $i < $param; $i++)
         {
             // Je vais ajouter au tableau un caractère spécial au hasard
             $password[] = $speciaux[rand(0,(count($speciaux) - 1))];
         }
+        // Je fais une boucle pour sélectionner les majuscules
         for ($i = 0; $i < $param; $i++)
         {
             // Je vais ajouter au tableau une majuscule au hasard
             $password[] = $majuscules[rand(0,count($majuscules) - 1)];
         }
+        // Je fais une boucle pour sélectionner les chiffres
         for ($i = 0; $i < $param; $i++)
         {
             // Je vais ajouter au tableau un chiffre au hasard
             $password[] = $chiffres[rand(0,count($chiffres) - 1)];
         }
+        // Je fais une boucle pour sélectionner les caractère normaux
+        for ($i = 0; $i < $param['longueur'] - $nbCarac; $i++)
+        {
+            // Je vais ajouter au tableau un caractère normal au hasard
+            $password[] = $chaine[rand(0,count($chaine) - 1)];
+        }
+        // Je mélange les éléments du tableau avec password
+        shuffle($password);
+        // Je transforme le tableau en chaine de caractère avec inplde();
+        $password = implode('', $password);
+        // Je retourne le mot de passe
+        return $password;
     }
+    // Si tout ne se passe pas comme prévu
+    return false;
 }
